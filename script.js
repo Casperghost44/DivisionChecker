e3891b4418bc1bda0e0aac5af16fc011a7188cdf
Casperghost44 <artemilk1989@mail.ru>
2024-02-16 21:59:33 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,71 @@
+"use strict";
+const inpData = document.querySelector(".inp-num");
+const numArr = document.querySelectorAll(".num-box");
+const btnAdd = document.querySelectorAll(".last-in-row");
+const btnRemove = document.querySelectorAll(".btn-hide");
+const containerEl = document.querySelector(".container");
+const spanEls = document.querySelectorAll("span");
+const darkIcon = document.querySelector(".dark-icon");
+const checkBtn = document.querySelector("#switch");
+const description = document.querySelector(".short-description");
+const footerEl = document.querySelector("footer");
+const cvLink = document.querySelector(".cv-link");
+
+inpData.addEventListener("keyup", function () {
+  clearNums();
+  const numVal = Number(inpData.value);
+  if (!Number.isFinite(numVal) || numVal < 0 || !Number.isInteger(numVal))
+    return;
+  checkDivisibility(numVal);
+});
+
+const checkDivisibility = function (numValue) {
+  numArr.forEach((num) => {
+    if (numValue % Number(num.textContent) === 0 && numValue != 0) {
+      num.classList.add("divisible");
+    }
+  });
+};
+
+const clearNums = function () {
+  numArr.forEach((num) => num.classList.remove("divisible"));
+};
+
+btnAdd.forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    if (btn.textContent !== "+") return;
+    btn.textContent = Number(btn.dataset.val);
+
+    document.querySelector(".row--inactive").classList.remove("row--inactive");
+    if (i > 0) {
+      btnRemove[i - 1].style.display = "none";
+    }
+  });
+});
+
+btnRemove.forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    btn.closest(".row").classList.add("row--inactive");
+    btnAdd[i].textContent = "+";
+    if (i > 0) {
+      btnRemove[i - 1].style.display = "flex";
+    }
+  });
+});
+
+checkBtn.checked = false;
+
+checkBtn.addEventListener("change", function () {
+  inpData.classList.toggle("dark");
+  numArr.forEach((item) => {
+    item.classList.toggle("dark");
+  });
+  spanEls.forEach((item) => {
+    item.classList.toggle("dark");
+  });
+  darkIcon.classList.toggle("dark");
+  containerEl.classList.toggle("dark");
+  description.classList.toggle("dark");
+  cvLink.classList.toggle("dark");
+  footerEl.classList.toggle("dark");
+});
